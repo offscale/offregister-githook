d23e627a50f398c171b9e05b69db35228e20c8d9
Samuel Marks <samuelmarks@gmail.com>
2017-05-03 10:59:40 +1000

    Added some [cursory] systemd support

--- offregister_githook/ubuntu/__init__.py
+++ offregister_githook/ubuntu/__init__.py
@@ -1,5 +1,8 @@
+from operator import le
 from os import path
 
+from offregister_fab_utils.misc import require_os_version
+from offregister_fab_utils.ubuntu.version import ubuntu_version
 from offutils import update_d, validate_conf
 from pkg_resources import resource_filename
 
@@ -22,9 +25,8 @@ def install_hookserve0(*args, **kwargs):
         sudo('chmod +x /usr/local/bin/{base}'.format(base=base))
 
 
-def setup_hookserve_upstart1(*args, **kwargs):
-    if exists('/run/systemd/system'):
-        raise NotImplementedError('SystemD not implemented yet')
+def setup_hookserve1(*args, **kwargs):
+    os_version = ubuntu_version()
 
     default_conf = {
         'AUTHOR': __author__,
@@ -35,26 +37,46 @@ def setup_hookserve_upstart1(*args, **kwargs):
         'PID': '/var/run/hookserve.pid'
     }
 
-    init_name = kwargs.get('hookserve-init-name', 'hookserve.conf')
-    init_dir = kwargs.get('hookserve-init-dir', '/etc/init')
-    init_local_filename = kwargs.get('hookserve-upstart-filename',
-                                     resource_filename('offregister_githook',
-                                                       path.join('conf', 'hookserve.upstart.conf')))
-    service = init_name.partition('.')[0]
-
     context = update_d(default_conf, kwargs.get('hookserve-init-context', {}))
     context['DAEMON_ARGS'] = context['DAEMON_ARGS'].format(DAEMON_PORT=context['DAEMON_PORT'])
 
-    upload_template(init_local_filename, '{init_dir}/{init_name}'.format(init_dir=init_dir, init_name=init_name),
-                    context=context, use_sudo=True)
+    if os_version < 15.04:
+        init_dir = kwargs.get('hookserve-init-dir', '/etc/init')
+        init_name = '{}.service'.format(
+            kwargs['hookserve-init-name']) if 'hookserve-init-name' in kwargs else 'hookserve.conf'
+        init_local_filename = kwargs.get('hookserve-upstart-filename',
+                                         resource_filename('offregister_githook',
+                                                           path.join('conf', 'hookserve.upstart.conf')))
+        service = init_name.partition('.')[0]
 
-    status_cmd = 'status {service}'.format(service=service)
-    if 'start/running' in run(status_cmd):
-        sudo('stop {service}'.format(service=service))
-    sudo('start {service}'.format(service=service))
-    return run(status_cmd)
+        upload_template(init_local_filename, '{init_dir}/{init_name}'.format(init_dir=init_dir, init_name=init_name),
+                        context=context, use_sudo=True)
+
+        status_cmd = 'status {service}'.format(service=service)
+        if 'start/running' in run(status_cmd):
+            sudo('stop {service}'.format(service=service))
+        sudo('start {service}'.format(service=service))
+        return run(status_cmd)
+    else:
+        unit_name = '{}.unit'.format(
+            kwargs['hookserve-init-name']) if 'hookserve-init-name' in kwargs else 'hookserve.unit'
+        unit_local_filename = kwargs.get('hookserve-upstart-filename',
+                                         resource_filename('offregister_githook',
+                                                           path.join('conf', 'hookserve.systemd.unit')))
+        service = unit_name.partition('.')[0]
+        unit_file = '/etc/systemd/system/{init_name}'.format(init_name=unit_name)
+
+        upload_template(unit_local_filename, unit_file, context=context, use_sudo=True)
+        sudo('chmod 664 {unit_file}'.format(unit_file=unit_file))
+        sudo('systemctl daemon-reload')
+        status_cmd = 'systemctl status {service}'.format(service=service)
+        if 'start/running' in run(status_cmd):
+            sudo('systemctl stop {service}'.format(service=service))
+        sudo('systemctl start {service}'.format(service=service))
+        return run(status_cmd)
 
 
+@require_os_version(15.04, le)
 def setup_git_pull_upstart2(*args, **kwargs):
     if exists('/run/systemd/system'):
         raise NotImplementedError('SystemD not implemented yet')
