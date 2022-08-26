68e6fa616def61a2874850973b3e5fc4bf263f65
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-25 22:06:30 -0400

    [*.py] Update to threadsafe Python 3 compatible Fabric ; [README.md] Update support Python version range ; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_githook/ubuntu/__init__.py
+++ offregister_githook/ubuntu/__init__.py
@@ -1,8 +1,8 @@
 from operator import le
 from os import path
 
-from fabric.contrib.files import exists, upload_template
-from fabric.operations import run, sudo
+from fabric.contrib.files import exists
+from fabric.operations import sudo
 from offregister_fab_utils.apt import apt_depends
 from offregister_fab_utils.misc import require_os_version
 from offregister_fab_utils.ubuntu.version import ubuntu_version
@@ -15,16 +15,16 @@ from offregister_githook import __author__, logger
 def install_hookserve0(*args, **kwargs):
     gz = "hookserve.gz"
     base = gz.partition(path.extsep)[0]
-    if not exists("/usr/local/bin/{base}".format(base=base)):
-        apt_depends("ca-certificates", "curl")
-        run("mkdir -p Downloads")
-        run(
+    if not exists(c, runner=c.run, path="/usr/local/bin/{base}".format(base=base)):
+        apt_depends(c, "ca-certificates", "curl")
+        c.run("mkdir -p Downloads")
+        c.run(
             "curl -L https://phayes.github.io/bin/current/hookserve/linux/{gz} -o Downloads/{gz}".format(
                 gz=gz
             )
         )
-        sudo("zcat Downloads/{gz} > /usr/local/bin/{base}".format(gz=gz, base=base))
-        sudo("chmod +x /usr/local/bin/{base}".format(base=base))
+        c.sudo("zcat Downloads/{gz} > /usr/local/bin/{base}".format(gz=gz, base=base))
+        c.sudo("chmod +x /usr/local/bin/{base}".format(base=base))
 
 
 def setup_hookserve1(*args, **kwargs):
@@ -59,7 +59,8 @@ def setup_hookserve1(*args, **kwargs):
         )
         service = init_name.partition(".")[0]
 
-        upload_template(
+        upload_template_fmt(
+            c,
             init_local_filename,
             "{init_dir}/{init_name}".format(init_dir=init_dir, init_name=init_name),
             context=context,
@@ -67,10 +68,10 @@ def setup_hookserve1(*args, **kwargs):
         )
 
         status_cmd = "status {service}".format(service=service)
-        if "start/running" in run(status_cmd):
-            sudo("stop {service}".format(service=service))
-        sudo("start {service}".format(service=service))
-        return run(status_cmd)
+        if "start/running" in c.run(status_cmd):
+            c.sudo("stop {service}".format(service=service))
+        c.sudo("start {service}".format(service=service))
+        return c.run(status_cmd)
     else:
         unit_name = (
             "{}.unit".format(kwargs["hookserve-init-name"])
@@ -86,22 +87,24 @@ def setup_hookserve1(*args, **kwargs):
         service = unit_name.partition(".")[0]
         unit_file = "/etc/systemd/system/{init_name}".format(init_name=unit_name)
 
-        upload_template(unit_local_filename, unit_file, context=context, use_sudo=True)
-        sudo("chmod 664 {unit_file}".format(unit_file=unit_file))
-        sudo("systemctl daemon-reload")
+        upload_template_fmt(
+            c, unit_local_filename, unit_file, context=context, use_sudo=True
+        )
+        c.sudo("chmod 664 {unit_file}".format(unit_file=unit_file))
+        c.sudo("systemctl daemon-reload")
         status_cmd = "systemctl status {service}".format(service=service)
-        if "start/running" in run(status_cmd):
-            sudo("systemctl stop {service}".format(service=service))
-        sudo("systemctl start {service}".format(service=service))
-        return run(status_cmd)
+        if "start/running" in c.run(status_cmd):
+            c.sudo("systemctl stop {service}".format(service=service))
+        c.sudo("systemctl start {service}".format(service=service))
+        return c.run(status_cmd)
 
 
 @require_os_version(15.04, le)
 def setup_git_pull_upstart2(*args, **kwargs):
-    if exists("/run/systemd/system"):
+    if exists(c, runner=c.run, path="/run/systemd/system"):
         raise NotImplementedError("SystemD not implemented yet")
 
-    apt_depends("inotify-tools")
+    apt_depends(c, "inotify-tools")
 
     default_conf = {
         "AUTHOR": __author__,
@@ -130,8 +133,8 @@ def setup_git_pull_upstart2(*args, **kwargs):
         name="git_pull-init-context",
     )
 
-    if not exists(kwargs["git_pull-init-context"]["GIT_DIR"]):
-        sudo(
+    if not exists(c, runner=c.run, path=kwargs["git_pull-init-context"]["GIT_DIR"]):
+        c.sudo(
             "git clone {git_repo} {to_dir}".format(
                 git_repo=kwargs["git_pull-init-context"]["GIT_REPO"],
                 to_dir=kwargs["git_pull-init-context"]["GIT_DIR"],
@@ -148,7 +151,8 @@ def setup_git_pull_upstart2(*args, **kwargs):
     )
     service = init_name.partition(".")[0]
 
-    upload_template(
+    upload_template_fmt(
+        c,
         init_local_filename,
         "{init_dir}/{init_name}".format(init_dir=init_dir, init_name=init_name),
         context=update_d(default_conf, kwargs.get("git_pull-init-context", {})),
@@ -156,8 +160,8 @@ def setup_git_pull_upstart2(*args, **kwargs):
     )
 
     status_cmd = "status {service}".format(service=service)
-    if "start/running" in run(status_cmd):
-        sudo("stop {service}".format(service=service))
+    if "start/running" in c.run(status_cmd):
+        c.sudo("stop {service}".format(service=service))
 
     logger.info(sudo("start {service}".format(service=service)))
-    return run(status_cmd)
+    return c.run(status_cmd)
